9a852c8b0115f0a99f4575b9a956494380cbf3f9
Christian Halim <chris.richard.halim@gmail.com>
2022-05-04 15:19:53 +0700

    [done] delete cart item

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -7,16 +7,29 @@ use Cart;
 
 class CartComponent extends Component
 {
-    public function increaseQuantity($id){
+    public function increaseQuantity($id)
+    {
         $product = Cart::get($id);
         $qty = $product->qty + 1;
         Cart::update($id, $qty);
     }
 
-    public function decreaseQuantity($id){
+    public function decreaseQuantity($id)
+    {
         $product = Cart::get($id);
         $qty = $product->qty - 1;
         Cart::update($id, $qty);
+    }
+
+    public function destroy($id)
+    {
+        Cart::remove($id);
+        session()->flash('success', 'Product removed from cart');
+    }
+
+    public function destroyAll() {
+        Cart::destroy();
+        session()->flash('success', 'Cart is emptied');
     }
 
     public function render()
