90c783b97a6698f630c7f3832d8f76afaa76329d
Christian Halim <chris.richard.halim@gmail.com>
2022-05-06 11:23:24 +0700

    [done] search product

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
-use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -19,14 +20,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('/', HomeComponent::class);
-
 
 Route::get('/shop', ShopComponent::class);
 
@@ -37,6 +37,8 @@ Route::get('/checkout', CheckoutComponent::class);
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
+
+Route::get('/search', SearchComponent::class)->name('product.search');
 
 // Route::middleware([
 //     'auth:sanctum',
