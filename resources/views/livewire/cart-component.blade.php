650bcedfca83da1b70ddc9adc70ce00f60597c4d
Christian Halim <chris.richard.halim@gmail.com>
2022-05-07 21:58:18 +0700

    [done] apply coupons

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -75,18 +75,55 @@
                     <h4 class="title-box">Order Summary</h4>
                     <p class="summary-info"><span class="title">Subtotal</span><b
                             class="index">${{ Cart::instance('cart')->subtotal() }}</b></p>
-                    <p class="summary-info"><span class="title">Tax</span><b
-                            class="index">${{ Cart::instance('cart')->tax() }}</b></p>
-                    <p class="summary-info"><span class="title">Shipping</span><b class="index">Free
-                            Shipping</b></p>
-                    <p class="summary-info total-info "><span class="title">Total</span><b
-                            class="index">${{ Cart::instance('cart')->total() }}</b></p>
+                    @if (Session::has('coupon'))
+                        <p class="summary-info"><span class="title">Discount
+                                ({{ Session::get('coupon')['code'] }}) <a href="#"
+                                    wire:click.prevent="removeCoupon"><i
+                                        class="fa fa-times text-danger"></i></a></span><b class="index">
+                                -${{ number_format($discount, 2) }}</b></p>
+                        <p class="summary-info"><span class="title">Subtotal with Discount</span><b
+                                class="index">${{ number_format($subtotalAfterDiscount, 2) }}</b></p>
+                        <p class="summary-info"><span class="title">Tax
+                                ({{ config('cart.tax') }}%)</span><b
+                                class="index">${{ number_format($taxAfterDiscount, 2) }}</b></p>
+                        <p class="summary-info total-info "><span class="title">Total</span><b
+                                class="index">${{ number_format($totalAfterDiscount, 2) }}</b></p>
+                    @else
+                        <p class="summary-info"><span class="title">Tax</span><b
+                                class="index">${{ Cart::instance('cart')->tax() }}</b></p>
+                        <p class="summary-info"><span class="title">Shipping</span><b
+                                class="index">Free
+                                Shipping</b></p>
+                        <p class="summary-info total-info "><span class="title">Total</span><b
+                                class="index">${{ Cart::instance('cart')->total() }}</b></p>
+                    @endif
                 </div>
                 <div class="checkout-info">
-                    <label class="checkbox-field">
-                        <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I
-                            have promo code</span>
-                    </label>
+                    @if (!Session::has('coupon'))
+                        <div style="margin-top: 10px;">
+                            <label class="checkbox-field">
+                                <input class="frm-input " name="have-code" id="have-code" value="1" type="checkbox"
+                                    wire:model="haveCouponCode"><span>Use
+                                    coupon code</span>
+                            </label>
+                        </div>
+
+                        @if ($haveCouponCode == 1)
+                            <div class="summary-item">
+                                <form wire:submit.prevent="applyCouponCode">
+                                    <h4 class="title-box">Coupon Code</h4>
+                                    @if (Session::has('error_message'))
+                                        <div class="alert alert-danger">{{ Session::get('error_message') }}</div>
+                                    @endif
+                                    <p class="row-in-form">
+                                        <label for="coupon-code">Enter your coupon code:</label>
+                                        <input type="text" name="coupon-code" wire:model="couponCode" />
+                                    </p>
+                                    <button type="submit" class="btn btn-small">Apply</button>
+                                </form>
+                            </div>
+                        @endif
+                    @endif
                     <a class="btn btn-checkout" href="checkout.html">Check out</a>
                     <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right"
                             aria-hidden="true"></i></a>
@@ -185,7 +222,8 @@
                                 </div>
                             </div>
                             <div class="product-info">
-                                <a href="#" class="product-name"><span>Radiant-360 R6 Wireless Omnidirectional Speaker
+                                <a href="#" class="product-name"><span>Radiant-360 R6 Wireless Omnidirectional
+                                        Speaker
                                         [White]</span></a>
                                 <div class="wrap-price"><ins>
                                         <p class="product-price">$168.00</p>
