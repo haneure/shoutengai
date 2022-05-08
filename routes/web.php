5a692de54e22bf068121dcc9bcf74090dd0e00a9
Christian Halim <chris.richard.halim@gmail.com>
2022-05-08 16:00:49 +0700

    [done] admin show orders

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
 use App\Http\Livewire\Admin\AdminHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminOrderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\Admin\AdminSaleComponent;
 use App\Http\Livewire\CartComponent;
@@ -101,4 +102,7 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
     Route::get('/admin/coupon', AdminCouponsComponent::class)->name('admin.coupons');
     Route::get('/admin/coupon/add', AdminAddCouponComponent::class)->name('admin.addcoupon');
     Route::get('/admin/coupon/edit{coupon_id}', AdminEditCouponComponent::class)->name('admin.editcoupon');
+
+    //Orders
+    Route::get('/admin/orders', AdminOrderComponent::class)->name('admin.orders');
 });
