edcbfd1731203cdb51fe38ace83b3051e5cc93d8
Christian Halim <chris.richard.halim@gmail.com>
2022-05-15 22:26:33 +0700

    heroku home category seed

---
+++ database/factories/HomeCategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HomeCategory>
+ */
+class HomeCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'sel_categories' => '1,2,3,4',
+            'no_of_products' => '8'
+        ];
+    }
+}
